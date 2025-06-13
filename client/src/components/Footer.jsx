70e1cf28718165f4358153da9bb908b49979c3d7
CLAIRE BOYLE <clairegboyle@gmail.com>
2025-06-13 19:35:40 +0100

    css: homepage styling update

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,26 @@
 import "./footer.css";
+import { Link } from "react-router";
 
 export default function Footer() {
   return (
     <>
-      <p>Home / Words / Quiz / Chat</p>
+      <nav className="footer">
+        <Link className="homelink" to={"/"}>
+          HOME
+        </Link>
+        <p className="divide"> / </p>
+        <Link className="learnlink" to={"/topic"}>
+          LEARN
+        </Link>
+        <p className="divide"> / </p>
+        <Link className="quizlink" to={"/quizpage"}>
+          QUIZ
+        </Link>
+        <p className="divide"> / </p>
+        <Link className="chatlink" to={"/chat"}>
+          CHAT
+        </Link>
+      </nav>
     </>
   );
 }
--- client/src/pages/homepage.jsx
+++ client/src/pages/homepage.jsx
@@ -1,10 +1,35 @@
 /* this is the homepage component 
 should have horizontal scrolling topics and conditionally rendered content underneath that
 equal to the "home" icon on the footer */
+import "./homepage.css";
 export default function Home() {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <section className="homepage">
+      <div className="homepagecolourblock">
+        <h1 className="homepagetitle">
+          Learning Irish? <h1 className="animated"></h1>
+        </h1>
+      </div>
+      <div className="homepagescroll">
+        <h2>
+          here will be a horizontal scrolling list of topics - a component
+        </h2>
+      </div>
+      <div className="irishbox">
+        <h3>DID YOU KNOW?</h3>
+        <ul>
+          <li>There are no words for yes or no in the Irish language.</li>
+          <li>
+            But there are three different number systems! One for maths, one for
+            counting people and one for counting anything non-human 🧑‍🏫
+          </li>
+          <li>There are also 45 different ways to say stone</li>
+          <li>
+            Irish is part of the 9% of languages that use Verb-Subject-Object
+            (like Spanish!)
+          </li>
+        </ul>
+      </div>
+    </section>
   );
 }
